a1605a399cf587323c76457753f26f301dfb215d
HOA1117 <hoaanhttt@gmail.com>
2025-06-05 23:49:40 +0700

    hoa send 2

---
+++ pjQuanLyHocPhi/Xoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjQuanLyHocPhi
+{
+    public partial class Xoa : Form
+    {
+        public Xoa()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_OK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.No;
+            this.Close();
+        }
+    }
+}
---
+++ pjQuanLyHocPhi/Xoa_Sai.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace pjQuanLyHocPhi
+{
+    partial class Xoa_Sai
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl_Thoat = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbl_NoiDung = new System.Windows.Forms.Label();
+            this.btn_OK = new Guna.UI2.WinForms.Guna2Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lbl_Thoat, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(729, 66);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // lbl_Thoat
+            // 
+            this.lbl_Thoat.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl_Thoat.AutoSize = true;
+            this.lbl_Thoat.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Thoat.ForeColor = System.Drawing.Color.White;
+            this.lbl_Thoat.Location = new System.Drawing.Point(281, 14);
+            this.lbl_Thoat.Name = "lbl_Thoat";
+            this.lbl_Thoat.Size = new System.Drawing.Size(166, 38);
+            this.lbl_Thoat.TabIndex = 0;
+            this.lbl_Thoat.Text = "Thông báo";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.lbl_NoiDung, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btn_OK, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 66);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(729, 221);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
+            // lbl_NoiDung
+            // 
+            this.lbl_NoiDung.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl_NoiDung.AutoSize = true;
+            this.lbl_NoiDung.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_NoiDung.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbl_NoiDung.Location = new System.Drawing.Point(82, 67);
+            this.lbl_NoiDung.Name = "lbl_NoiDung";
+            this.lbl_NoiDung.Size = new System.Drawing.Size(565, 32);
+            this.lbl_NoiDung.TabIndex = 2;
+            this.lbl_NoiDung.Text = "Không thể xóa giảng viên, vui lòng kiểm tra lại !";
+            this.lbl_NoiDung.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_OK.BorderRadius = 10;
+            this.btn_OK.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_OK.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_OK.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_OK.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_OK.FillColor = System.Drawing.Color.SteelBlue;
+            this.btn_OK.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_OK.ForeColor = System.Drawing.Color.White;
+            this.btn_OK.Location = new System.Drawing.Point(274, 148);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(180, 47);
+            this.btn_OK.TabIndex = 3;
+            this.btn_OK.Text = "OK";
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // Xoa_Sai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(729, 287);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Xoa_Sai";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xoa_Sai";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lbl_Thoat;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label lbl_NoiDung;
+        private Guna.UI2.WinForms.Guna2Button btn_OK;
+    }
+}
